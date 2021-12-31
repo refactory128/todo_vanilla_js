91564ed5313ae3a4786a0d9081114ab53e59148b
WebWorflowSolutions99 <83876343+WebWorflowSolutions99@users.noreply.github.com>
2021-12-31 11:52:18 -0600

    funcitonality to add and delete projects

--- src/index.js
+++ src/index.js
@@ -16,15 +16,17 @@ class user {
         userName = 'Default User',
     ) {
         this.userName = userName;
+        this.parent = null;
         this.subElements = new Array();
     }
 }
 
-user.prototype.add = function(subElement) {
+user.prototype.addSubElement = function(subElement) {
     this.subElements.push(subElement);
 }
 
-user.prototype.remove = function(subElement) {
+user.prototype.removeSubElement = function(subElement) {
+    console.log(this.subElements);
     this.subElements.splice(this.subElements.indexOf(subElement),1);
 }
 
@@ -36,21 +38,30 @@ user.prototype.getData = function() {
     return {userName: this.userName};
 }
 
+user.prototype.getParent = function(){
+    return this.parent;
+}
+
 /////////////////////////////////////////
 class project {
     constructor (
         title = '',
+        parent = null,
     ) {
         this.title = title;
         this.subElements = new Array();
+        this.parent = parent;
+        if (parent !== null){
+            parent.addSubElement(this);
+        }
     }
 }
 
-project.prototype.add = function(subElement) {
+project.prototype.addSubElement = function(subElement) {
     this.subElements.push(subElement);
 }
 
-project.prototype.remove = function(subElement) {
+project.prototype.removeSubElement = function(subElement) {
     this.subElements.splice(this.subElements.indexOf(subElement),1);
 }
 
@@ -62,6 +73,13 @@ project.prototype.update = function (userInput){
     this.title = userInput.title;
 }
 
+project.prototype.delete = function (){
+    const parent = this.getParent();
+    console.log("parent = " );
+    console.log(parent);
+    parent.removeSubElement(this);
+}
+
 project.prototype.getSubElements = function() {
     return this.subElements;
 }
@@ -70,6 +88,9 @@ project.prototype.getData = function() {
     return {title: this.title};
 }
 
+project.prototype.getParent = function(){
+    return this.parent;
+}
 
 //////////////////////////////////////////
 class task {
@@ -78,13 +99,18 @@ class task {
         description = '',
         priority = 0,
         dueDate = '2021-01-01',
-        complete = false
+        complete = false,
+        parent = null,
     ) {
         this.title = title;
         this.description = description;
         this.priority = priority;
         this.dueDate = dueDate;
         this.complete = complete;
+        this.parent = parent;
+        if (parent !== null){
+            parent.addSubElement(this);
+        }
     }
 }
 
@@ -115,6 +141,14 @@ task.prototype.getData = function() {
             priority: this.priority,
             dueDate: this.dueDate,
             complete: this.complete };
+}
+
+task.prototype.getParent = function(){
+    return this.parent;
+}
+
+task.prototype.delete = function (){
+    this.getParent().removeSubElement(this);
 }
 
 //////////////////////////////////////////
@@ -191,11 +225,13 @@ function generateModal(fields, element, callback) {
 //fields data for render
 //Remove content button callback
 //Edit content button callback
-//renderAs -- render as project or task (optional task default)
+//element to edit or delete for callback
+//highlight -- render in primary color or secondary color
 
-function renderContent(fields) {  
+function renderContent(fields, deleteCallback, editCallback, element, highlight = false) {  
     const div = document.createElement("div");
     div.classList.add('element');
+    if (highlight) {  div.classList.add('highlight')};
 
     let divHtml =""
     for (const field in fields){
@@ -208,8 +244,18 @@ function renderContent(fields) {
     button.innerText = "Delete"
     button.addEventListener("click", e =>{
         //todo add remove element callback
+        deleteCallback(element);
     });
     div.appendChild(button);
+
+    const button2 = document.createElement("button");
+    button2.innerText = "Edit"
+    button2.addEventListener("click", e =>{
+        //todo add remove element callback
+        editCallback();
+    });
+    div.appendChild(button2);
+
     let body = document.querySelector("body");
     body.appendChild(div);
 }
@@ -222,8 +268,7 @@ function renderContent(fields) {
 //////////////////////////////////////////
 
 function createNewProject()  {
-    let tempProject = new project();
-    user1.add(tempProject);
+    let tempProject = new project('New Project',user1);
     generateModal(tempProject.getFillableFields(), tempProject, updateElement);
 
 }
@@ -238,17 +283,38 @@ function updateElement(element,userInput)
 
 }
 
+
+
+function removeElement(element)
+{
+    console.log("remove element");
+    console.log(element);
+
+
+    
+    element.delete();
+    renderPage();
+}
+
+
 function renderPage()
 {
     clearPage();
     navBar(createNewProject);  //generate nav bar and call back to createNewProject when plus is clicked
     
+    //let user = user1;
     let elements = user1.getSubElements();
-    console.log(elements);
+    
+    //console.log(user);
     
     for(const element of elements) {
-        renderContent(element.getData());
-
+        renderContent(element.getData(),
+                        removeElement,
+                        element.update,
+                        element,
+                      true);
+        //user1.removeSubElement(element);
+        console.log(user1);
     }
     
 }
@@ -256,7 +322,8 @@ function renderPage()
 /////////////////////////////////////////
 //Main program starts here
 
-let user1 = new user("user1");
+const user1 = new user("user1");
+let project1 = new project('project1',user1);
 
 renderPage();
 //create nav bar for user 1
